566705e64f3f3e6a7f86f5a3dd6b552cd1f5f282
lindabarbaratg <lindabarbaratg@gmail.com>
2025-03-21 23:22:08 +0700

    Update run3.py

--- run3.py
+++ run3.py
@@ -22,11 +22,11 @@ def read_csv_range(filename, start, end):
     return rows
 
 # Rentang data yang diproses (misal dari baris 1 sampai 50)
-start_row = 600  # Baris pertama (0-based index)
-end_row = 800  # Baris terakhir yang ingin diproses
+start_row = 800  # Baris pertama (0-based index)
+end_row = 1000  # Baris terakhir yang ingin diproses
 
 # Deklarasi akun tunggal
-email = "SherlydwiAriyani"
+email = "yowicagustina"
 password = "@@Dayungh332"
 
 # Baca judul video sesuai rentang yang diinginkan
